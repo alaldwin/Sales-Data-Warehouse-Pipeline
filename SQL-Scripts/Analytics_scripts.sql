7c337076c8c8ee79cb4a97729384315973b6c71e
Aldwin <magpantay01258@gmail.com>
2026-07-01 18:36:36 +0800

    Update Analytics_scripts.sql

--- SQL-Scripts/Analytics_scripts.sql
+++ SQL-Scripts/Analytics_scripts.sql
@@ -1,17 +1,24 @@
 -- Total Sales Volume by Country
 SELECT
 	RANK() OVER (
-		ORDER BY SUM(o.totalamount) DESC
+		ORDER BY SUM(fo.totalamount) DESC
 	) AS sales_rank,
-    c.country,
-    SUM(o.totalamount) AS total_sales,
-    AVG(o.shippingcost) AS avg_shipping,
-    COUNT(o.orderid) AS order_count
-FROM star_schema.fact_orders AS o
-INNER JOIN star_schema.dim_customer AS c
-    ON o.customerid = c.customerid
+    dc.country,
+    SUM(fo.totalamount) AS total_sales,
+    ROUND(
+		AVG(
+		fo.shippingcost), 
+		2
+	) AS avg_shipping,
+    COUNT(fo.orderid) AS order_count
+FROM 
+	star_schema.fact_orders AS fo
+INNER JOIN 
+	star_schema.dim_customer AS dc
+    ON 
+		fo.customerid = dc.customerid
 GROUP BY
-    c.country
+    dc.country
 ORDER BY
     total_sales DESC;
 
@@ -30,9 +37,12 @@ SELECT
 	d.year,
 	d.month_name,
 	SUM(o.totalamount) AS monthly_sales
-FROM star_schema.fact_orders AS o
-INNER JOIN star_schema.dim_date AS d
-    ON o.orderdate = d.dateid
+FROM 
+	star_schema.fact_orders AS o
+INNER JOIN 
+	star_schema.dim_date AS d
+    ON 
+		o.orderdate = d.dateid
 GROUP BY
     d.year,
     d.month_name
@@ -52,9 +62,12 @@ SELECT
 	) AS yearly_sales_rank,
 	d.year,
 	SUM(o.totalamount) AS yearly_sales
-FROM star_schema.fact_orders AS o
-INNER JOIN star_schema.dim_date AS d
-	ON o.orderdate = d.dateid
+FROM 
+	star_schema.fact_orders AS o
+INNER JOIN 
+	star_schema.dim_date AS d
+	ON 
+		o.orderdate = d.dateid
 GROUP BY
 	d.year
 ORDER BY 
